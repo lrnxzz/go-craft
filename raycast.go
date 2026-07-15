5295247315e27cc5455a39367be2698d919d18f7
lorenzo-losi <lrxnzxc@proton.me>
2026-07-15 01:52:54 -0300

    scope the raycast helpers to the traversal

--- raycast.go
+++ raycast.go
@@ -16,10 +16,29 @@ func (w *World) Raycast(origin, direction Vec3d, reach float64, solid func(Block
 		return RayHit{}, false
 	}
 
+	march := func(origin, direction float64) (int, float64, float64) {
+		switch {
+		case direction > 0:
+			return 1, (math.Floor(origin) + 1 - origin) / direction, 1 / direction
+		case direction < 0:
+			return -1, (origin - math.Floor(origin)) / -direction, -1 / direction
+		default:
+			return 0, math.Inf(1), math.Inf(1)
+		}
+	}
+
+	entered := func(step int, positive, negative BlockFace) BlockFace {
+		if step > 0 {
+			return positive
+		}
+
+		return negative
+	}
+
 	block := origin.Floor()
-	stepX, tMaxX, tDeltaX := axisMarch(origin.X, direction.X)
-	stepY, tMaxY, tDeltaY := axisMarch(origin.Y, direction.Y)
-	stepZ, tMaxZ, tDeltaZ := axisMarch(origin.Z, direction.Z)
+	stepX, tMaxX, tDeltaX := march(origin.X, direction.X)
+	stepY, tMaxY, tDeltaY := march(origin.Y, direction.Y)
+	stepZ, tMaxZ, tDeltaZ := march(origin.Z, direction.Z)
 
 	for {
 		var (
@@ -32,17 +51,17 @@ func (w *World) Raycast(origin, direction Vec3d, reach float64, solid func(Block
 			distance = tMaxX
 			tMaxX += tDeltaX
 			block.X += stepX
-			face = enteredFace(stepX, FaceWest, FaceEast)
+			face = entered(stepX, FaceWest, FaceEast)
 		case tMaxY <= tMaxZ:
 			distance = tMaxY
 			tMaxY += tDeltaY
 			block.Y += stepY
-			face = enteredFace(stepY, FaceDown, FaceUp)
+			face = entered(stepY, FaceDown, FaceUp)
 		default:
 			distance = tMaxZ
 			tMaxZ += tDeltaZ
 			block.Z += stepZ
-			face = enteredFace(stepZ, FaceNorth, FaceSouth)
+			face = entered(stepZ, FaceNorth, FaceSouth)
 		}
 
 		if distance > reach {
@@ -65,23 +84,4 @@ func (w *World) Raycast(origin, direction Vec3d, reach float64, solid func(Block
 			Distance: distance,
 		}, true
 	}
-}
-
-func axisMarch(origin, direction float64) (step int, tMax, tDelta float64) {
-	switch {
-	case direction > 0:
-		return 1, (math.Floor(origin) + 1 - origin) / direction, 1 / direction
-	case direction < 0:
-		return -1, (origin - math.Floor(origin)) / -direction, -1 / direction
-	default:
-		return 0, math.Inf(1), math.Inf(1)
-	}
-}
-
-func enteredFace(step int, positive, negative BlockFace) BlockFace {
-	if step > 0 {
-		return positive
-	}
-
-	return negative
 }
